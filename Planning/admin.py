9ca31d81f8f86436a4726e17868dd60abdbf8400
Anton <antonsvz@gmail.com>
2018-07-01 01:32:11 +0300

    Fiest change

--- Planning/admin.py
+++ Planning/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Period, CurrentBalance, ChangeBalance
 
+
+class PeriodAdmin(admin.ModelAdmin):
+    list_display = ("name", "start_Date", "end_Date")
+
+
+class ChangeBalanceAdmin(admin.ModelAdmin):
+    list_display = ("sum", "category", "change")
+
+
+admin.site.register(Period, PeriodAdmin)
+admin.site.register(CurrentBalance)
+admin.site.register(ChangeBalance, ChangeBalanceAdmin)
 # Register your models here.
--- Planning/models.py
+++ Planning/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from datetime import date
+
+
+class Period(models.Model):
+    name = models.CharField("Название:", max_length=30, null=True, help_text="Например: название месяца, номер  и т.п.")
+    start_Date = models.DateField("Начало периода:", default=date.today)
+    end_Date = models.DateField("Конец периода:", default=date.today)
+
+    def __str__(self):
+        return self.name
+
+
+class CurrentBalance(models.Model):
+    total = models.CharField("Текущий баланс", default=0, max_length=10)
+
+
+class ChangeBalance(models.Model):
+    sum = models.DecimalField("Сумма:", decimal_places=2, max_digits=10)
+    name = models.CharField("Название:", max_length=30, null=True)
+    category = models.CharField("Категория:", max_length=30, null=True)
+    date = models.DateField("Дата")
+    choises_balans = (
+        ("Spending", "Расход"),
+        ("Income", "Поступление")
+    )
+    change = models.CharField(max_length=8, choices=choises_balans, default="Income")
+
+    period = models.ForeignKey(Period)
+
+    def __str__(self):
+        return self.get_change_display() + ': ' + str(self.sum)
 
 # Create your models here.

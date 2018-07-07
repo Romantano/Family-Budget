1a9689440762b18b1ea2955d393ce627e96dbd88
Anton <antonsvz@gmail.com>
2018-07-08 02:17:51 +0300

    bdfbd

--- Planning/models.py
+++ Planning/models.py
@@ -12,23 +12,24 @@ class Period(models.Model):
 
 
 class CurrentBalance(models.Model):
-    total = models.CharField("Текущий баланс", default=0, max_length=10)
+    total = models.DecimalField("Текущий баланс", decimal_places=2, max_digits=10, blank=True)
 
 
 class ChangeBalance(models.Model):
     sum = models.DecimalField("Сумма:", decimal_places=2, max_digits=10)
     name = models.CharField("Название:", max_length=30, null=True)
     category = models.CharField("Категория:", max_length=30, null=True)
-    date = models.DateField("Дата")
     choises_balans = (
         ("Spending", "Расход"),
         ("Income", "Поступление")
     )
     change = models.CharField(max_length=8, choices=choises_balans, default="Income")
 
-    period = models.ForeignKey(Period)
+ #   period = models.ForeignKey(Period)
 
-    def __str__(self):
-        return self.get_change_display() + ': ' + str(self.sum)
+ #   def __str__(self):
+  #      return self.get_change_display() + ': ' + str(self.sum)
+
+
 
 # Create your models here.
--- Planning/views.py
+++ Planning/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django import forms
+from django.forms import RadioSelect
+from django.shortcuts import render, redirect
+from Planning.models import ChangeBalance, CurrentBalance
+
 
 # Create your views here.
+
+
+class ChangeBalanceForm(forms.ModelForm):
+    class Meta:
+        model = ChangeBalance
+        fields = ('sum', 'name', 'category', 'change')
+
+
+def index(request):
+    cash = CurrentBalance.objects.get(id=1)
+    table = ChangeBalance.objects.all()
+    if request.method == "POST":
+        form = ChangeBalanceForm(request.POST)
+        if form.is_valid():
+            change_balance = ChangeBalance()
+            data = form.cleaned_data
+            change_balance.sum = data['sum']
+            change_balance.name = data['name']
+            change_balance.category = data['category']
+            change_balance.change = data['change']
+            change_balance.save()
+            if data['change'] == 'Spending':
+                cash.total -= data['sum']
+            else:
+                cash.total += data['sum']
+            cash.save()
+            return redirect("/")
+    else:
+        form = ChangeBalanceForm(initial={"change": RadioSelect})
+    return render(request, "index.html", {'form': form, 'total': cash.total, 'table': table})
